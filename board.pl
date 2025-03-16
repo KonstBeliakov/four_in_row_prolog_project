56a9b1bfa1158ea003a0ec69e4ff69d543e20c04
KonstBeliakov <konstbeliakov@gmail.com>
2025-03-16 11:38:32 +0200

    refactoring (code in two files)

---
+++ board.pl
@@ -0,0 +1,68 @@
+:- dynamic(list_element_2d/3).
+:- dynamic(list_sizeX/1).
+:- dynamic(list_sizeY/1).
+
+generate_list_row_helper(RowNumber, Y) :-
+    list_sizeY(SizeY),
+    Y < SizeY,
+    assertz(list_element_2d(RowNumber, Y, 0)),
+    Y1 is Y + 1,
+    generate_list_row_helper(RowNumber, Y1).
+
+generate_list_row_helper(_, Y) :-   % когда X >= SizeX, ничего не делаем.
+    list_sizeY(SizeY),
+    Y >= SizeY.
+
+generate_list_row(RowNumber) :- generate_list_row_helper(RowNumber, 0).
+
+generate_list_helper(X) :-
+    list_sizeX(SizeX),
+    X < SizeX,
+    generate_list_row(X),
+    X1 is X + 1,
+    generate_list_helper(X1).
+
+generate_list_helper(X) :-
+    list_sizeX(SizeX),
+    X >= SizeX.
+
+generate_list(_) :- generate_list_helper(0).
+
+show_list_row_helper(X, Y) :-
+    list_sizeY(SizeY),
+    (Y < SizeY ->
+    	list_element_2d(X, Y, Element),
+    	write(Element), write(' '),
+    	Y1 is Y + 1,
+    	show_list_row_helper(X, Y1);
+
+    	nl
+    ).
+
+show_list_row_helper(Y) :-   % когда X >= SizeX, ничего не делаем
+    list_sizeY(SizeY),
+    Y >= SizeY,
+    nl.
+
+show_list_row(X) :- show_list_row_helper(X, 0).
+
+show_list_helper(X) :-
+    list_sizeX(SizeX),
+    X < SizeX,
+    (show_list_row(X); write('\n')),
+    write('\n'),
+    X1 is X + 1,
+    show_list_helper(X1).
+
+show_list_helper(X) :-
+    list_sizeX(SizeX),
+    X >= SizeX.
+
+show_list(_) :- show_list_helper(0).
+
+set_element(X, Y, Value) :-
+    retractall(list_element_2d(X, Y, _)),
+    assertz(list_element_2d(X, Y, Value)).
+
+get_element(X, Y, Value) :-
+    list_element_2d(X, Y, Value).
